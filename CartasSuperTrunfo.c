45c42d36ce646721ae8073ecb84a5a8b14ab5987
JpBllack <joaogomes@unitins.br>
2025-02-14 17:11:58 -0300

    modifiquei de novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,48 +1,75 @@
-#include <stdio.h>
-#include <locale.h>
-#include <wchar.h>
+    #include <stdio.h>
+    #include <locale.h>
+    #include <wchar.h>
+    #include <string.h>
 
-int main() {
-    setlocale(LC_ALL, "Portuguese");
+    #define MAX_CIDADES 32
 
-    int codigoCidade;
-    wchar_t nome[50];
-    int populacao;
-    float area;
-    float pib;
-    int pontosTuristicos;
+    typedef struct
+    {
+        char codigoCidade[4];
+        wchar_t nome[50];
+        int populacao;
+        float area;
+        float pib;
+        int pontosTuristicos;
+    } Cidade;
 
-    wprintf(L"Super Trunfo - Cadastro de Cartas\n");
-    wprintf(L"------------------------------------\n");
+    int main()
+    {
+        setlocale(LC_ALL, "Portuguese");
 
-    wprintf(L"Digite o Código da Cidade: ");
-    scanf("%d", &codigoCidade);
-    getchar();
+        Cidade cidades[MAX_CIDADES];
 
-    wprintf(L"Digite o Nome da Cidade: ");
-    fgetws(nome, sizeof(nome)/ sizeof(nome[0]), stdin);
-    nome[wcslen(nome) - 1] = L'\0';
+        wprintf(L"Super Trunfo - Cadastro de Cartas\n");
+        wprintf(L"------------------------------------\n");
 
-    wprintf(L"Digite o Tamanho da População (EX: 2000000): ");
-    scanf("%d", &populacao);
+        for (int i = 0; i < MAX_CIDADES; i++)
+        {
+            wprintf(L"\nCadastro da cidade %d/%d:\n", i + 1, MAX_CIDADES);
+            
+            // Gerando código automaticamente (A01, A02, ..., H04)
+            char estado = 'A' + (i / 4);
+            int cidadeNum = (i % 4) + 1;
+            sprintf(cidades[i].codigoCidade, "%c%02d", estado, cidadeNum);
+            
+            wprintf(L"Nome da Cidade: ");
+            fgetws(cidades[i].nome,sizeof(cidades[i].nome) / sizeof(wchar_t),stdin);
 
-    wprintf(L"Digite o Tamanho da Área (em km²): ");
-    scanf("%f", &area);
+            size_t len = wcslen(cidades[i].nome);
+            if (len > 0 && cidades[i].nome[len - 1] == L'\n') 
+            {
+                cidades[i].nome[len - 1] = L'\0';
+            }
+            
+            wprintf(L"Tamanho da População (EX: 2000000): ");
+            scanf("%d", &cidades[i].populacao);
 
-    wprintf(L"Digite o PIB da Cidade (em bilhões): ");
-    scanf("%f", &pib);
+            wprintf(L"Tamanho da Área (em km²): ");
+            scanf("%f", &cidades[i].area);
 
-    wprintf(L"Digite a Quantidade de Pontos Turísticos: ");
-    scanf("%d", &pontosTuristicos);
+            wprintf(L"PIB da Cidade (em bilhões): ");
+            scanf("%f", &cidades[i].pib);
 
-    // Exibição dos dados cadastrados
-    wprintf(L"\nOs valores inseridos para cada atributo da cidade foram:\n");
-    wprintf(L"Código da Cidade: %d\n", codigoCidade);
-    wprintf(L"Nome: %ls\n", nome);
-    wprintf(L"População: %d\n", populacao);
-    wprintf(L"Área: %.2f km²\n", area);
-    wprintf(L"PIB: %.2f bilhões\n", pib);
-    wprintf(L"Pontos Turísticos: %d\n", pontosTuristicos);
+            wprintf(L"Digite a Quantidade de Pontos Turísticos: ");
+            scanf("%d", &cidades[i].pontosTuristicos);
+            getchar();
+        }
 
-    return 0;
-}
+        // Exibição dos dados cadastrados
+        wprintf(L"\n--- Cidades Cadastradas ---\n");
+        for (int i = 0; i < MAX_CIDADES; i++)
+        {
+            wprintf(L"Código: %s\n", &cidades[i].codigoCidade);
+            wprintf(L"Nome: %ls\n", &cidades[i].nome);
+            wprintf(L"População: %d\n", &cidades[i].populacao);
+            wprintf(L"Área: %.2f km²\n", &cidades[i].area);
+            wprintf(L"PIB: %.2f bilhões\n", &cidades[i].pib);
+            wprintf(L"Pontos Turísticos: %d\n", &cidades[i].pontosTuristicos);
+            wprintf(L"------------------------------------------------------\n");
+            
+        }
+        
+
+        return 0;
+    }
