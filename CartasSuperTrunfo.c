611ba3edd1c58a39bcb87ddd73df6a409cb0c7bc
JpBllack <joaogomes@unitins.br>
2025-02-14 11:17:03 -0300

    atividade incimpleta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,42 @@
 //Teste larissa
 
 int main() {
+    int codigoCidade;
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+
+    printf("Super Trunfo - Cadastro de Cartas\n");
+
+    printf("Digite o Codigo da Cidade:");
+    printf("%d", &codigoCidade);
+
+    printf("Digite o Nome da Cidade:");
+    scanf("%[^\n]", nome);
+
+    printf("Digite o Tamanho da população(EX:2000000):");
+    scanf("%d",&populacao);
+
+    printf("Digite o Tamanho da Aréa:");
+    scanf("%f", &area);
+    printf("Digite o PIB da Cidade:");
+    scanf("%f", &pib);
+    printf("Digite a quantidade de pontos Turisticos:");
+    scanf("%d", &pontosTuristicos);
+
+     
+    printf("\nOs valores inseridos para cada atributo da cidade foram:\n");
+    printf("Código da Cidade: %d\n", codigoCidade);
+    printf("Nome: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões\n", pib);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos);
+    
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
